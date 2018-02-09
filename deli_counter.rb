7eb97e153005bfe98cfbc32b8bc4e23fd1bd4ec7
chandana M <chandu11288@gmail.com>
2018-02-09 14:40:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,12 @@ def line(katz_deli)
     puts "The line is currently empty"
   end
 end
+
 def take_a_number(katz_deli,"name")
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli[index+1]} in line."
+  end
 end
+
 def now_serving()
 end
