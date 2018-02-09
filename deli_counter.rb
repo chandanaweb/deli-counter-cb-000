82193db9374145633b852f9bced010107873ba87
chandana M <chandu11288@gmail.com>
2018-02-09 15:26:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length < 1
     puts "The line is currently empty."
+  else
+    
   end
 end
 
@@ -11,6 +13,10 @@ def take_a_number(katz_deli,name)
   puts "Welcome, #{name}. You are number #{index+1} in line."
 end
 
-def now_serving()
-
+def now_serving(katz_deli)
+  if katz_deli.length < 1
+    puts "There is nobody waiting to be served!."
+  else
+    
+  end
 end
