7e349d1ea316c9ce8224bf356df28744105452ee
chandana M <chandu11288@gmail.com>
2018-02-09 15:44:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index { |name,index|
-      katz_deli[index] = index.conjugate( "." + " " + katz_deli[index])
+      num = index+1
+      katz_deli[index] = num.join+ "." + " " + katz_deli[index]
     }
     katz_deli.join(" ")
    puts "The line is currently: #{katz_deli}"
