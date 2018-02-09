a80a40d34014531de802008bb1b068907db1a800
chandana M <chandu11288@gmail.com>
2018-02-09 14:39:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length < 1
+    puts "The line is currently empty"
+  end
+end
+def take_a_number(katz_deli,"name")
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli[index+1]} in line."
+end
+def now_serving()
+end
